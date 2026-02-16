236311ddca505b8039873ed0071b22499bb24239
GitHub Action <action@github.com>
2026-02-16 13:28:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/cashpilotthrivehue/dreamlife/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/cashpilotthrivehue/dreamlife/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.cashpilotthrivehue.dreamlife.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
